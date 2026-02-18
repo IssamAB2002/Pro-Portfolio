852f0b129e54a22c2e13485f59d345d414aa5cc5
Issam <abbasissam98@gmail.com>
2026-02-18 23:09:09 +0100

    fix static and blank page

--- backend/pro_portfolio/settings.py
+++ backend/pro_portfolio/settings.py
@@ -90,7 +90,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static/react",
 ]
 
 # --- WHITENOISE CONFIGURATION ---
